8a3661dea910066dfc3112febde375d93ac25bfa
InternalHigh <internalhigh@vodafonemail.de>
2026-05-19 21:31:25 +0200

    -

--- Source/Streamrecorder.hpp
+++ Source/Streamrecorder.hpp
@@ -138,6 +138,7 @@ private:
 	static void OpenFolder(const std::string& path)
 	{
 		wxFileName pathBuilder(path);
+		pathBuilder.Normalize();
 		wxString folderPath(pathBuilder.GetFullPath());
 
 		wxFileName::Mkdir(folderPath, wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL);
